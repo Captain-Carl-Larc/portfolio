681608f103f93586a20cf666eb5dd9feb03f31ec
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-04-23 20:40:03 +0300

    Navbar set up

---
+++ carl/src/app/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+    return ( 
+        <nav>
+            <h1 className="text-center text-3xl font-bold text-white min-h-20">
+                Nav bar here
+            </h1>
+        </nav>
+     );
+}
+
+export default Navbar;
