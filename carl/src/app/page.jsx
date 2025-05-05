c9a0aad881904d1005726860943e040891e77bdc
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-05-05 14:32:36 +0300

    Skills section done

--- carl/src/app/page.jsx
+++ carl/src/app/page.jsx
@@ -1,12 +1,16 @@
 import HeroSection from "./components/Hero";
 import AboutSection from "./components/About"
+import Footer from "./components/Footer";
+import Skills from "./components/Skills";
 //import TechnicalSkillsSection from "./components/Skills"
 export default function Home() {
   return (
     <>
       <section className="">
         <HeroSection />
-        <AboutSection/>        
+        <AboutSection/>
+        <Skills/>
+        <Footer/>     
       </section>
     </>
   );
